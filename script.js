71747f7beaa3a116c4f0fe766f056f546c04e77d
Geanderson12 <geanderson12@gmail.com>
2023-04-20 01:58:37 -0300

    versao 1.0

---
+++ script.js
@@ -0,0 +1,42 @@
+const carousel = document.querySelectorAll('.card');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlide() {
+    carousel.forEach(item => item.classList.remove('on'))
+}
+
+function showSlide() {
+    carousel[currentSlide].classList.add('on')
+}
+
+
+function nextSlide() {
+    hideSlide()
+    if (currentSlide === carousel.length - 1) {
+        currentSlide = 0
+    } else {
+        currentSlide++
+    }
+    showSlide()
+}
+
+function prevSlide() {
+    hideSlide()
+    if (currentSlide === 0) {
+        currentSlide = carousel.length -1
+    } else {
+        currentSlide--
+    }
+    showSlide()
+}
+
+/*
+btnNext.addEventListener('click', ()=> alert('clicou'))
+btnPrev.addEventListener('click', ()=> alert('clicou'))
+*/
+
+btnNext.addEventListener('click', nextSlide)
+btnPrev.addEventListener('click', prevSlide)
